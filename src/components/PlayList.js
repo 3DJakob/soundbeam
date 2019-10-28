b14fcea218e05d37383da56e0234c2c4b1631327
Jakob <jakob@Jakobs-MacBook-Air.local>
2019-10-28 10:34:01 +0100

    Working playlist component
    Working playlist component with a neat design with artist name, song name, plays and if liked.

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import millify from 'millify'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
-function Track ({info}) {
-    return <div>{info.title}</div>
+
+function Track ({trackInfo}) {
+    console.log(trackInfo)
+    const count = millify(trackInfo.track.playback_count)
+    return (
+        <div className='track'>
+            <p>{trackInfo.track.user.username}</p>
+            <p>{trackInfo.track.title}</p>
+            <p>{count}</p>
+            {trackInfo.kind === 'like' ? <FontAwesomeIcon style={{color: '#fff'}} icon={faHeart} /> : ''}
+        </div>
+    )
 }
 
 function PlayList ({playlist}) {
+    // console.log(playlist[0].artwork_url)
     console.log(playlist)
 
     return (
-        <div>
-            Playlist
-
-            <div>
-                {playlist.map(track => <Track key={track.id} info={track} />)}
-            </div>
+        <div className='playlist'>
+            {playlist.map(trackInfo => <Track key={trackInfo.track.id} trackInfo={trackInfo} />)}
+            <div className='blurredBg' style={{backgroundImage: `url(${playlist[2].track.artwork_url})`}}></div>
         </div>
     )
 }
